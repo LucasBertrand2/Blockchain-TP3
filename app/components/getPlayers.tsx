dfabf4edb9cfaa9950cac7eafe6161f326dac191
lucas bertrand <lucas.bertrand@epitech.eu>
2024-04-14 18:43:20 -0400

    Ajout d'un texte si y'a personne qui a encore acheté un ticket

--- app/components/getPlayers.tsx
+++ app/components/getPlayers.tsx
@@ -359,7 +359,8 @@ interface Window {
 }
 
 const GetPlayers = () => {
-    const [playersData, setPlayersData] = useState<any[]>([]);
+    const [playersData, setPlayersData] = useState<string[]>([]);
+    const [buttonClicked, setButtonClicked] = useState<boolean>(false);
 
     const handleGetPlayersData = async () => {
         try {
@@ -367,6 +368,7 @@ const GetPlayers = () => {
             const Web3 = new LotteryService('0xe68A3c8FF152C8113104658Dae1BD9a1d1c1bD3d', JSON.stringify(ABI), myWindow.ethereum);
             const players = await Web3.getPlayers();
             setPlayersData(players);
+            setButtonClicked(true);
             console.log('Players data:', JSON.stringify(players));
         } catch (error) {
             console.error('Error fetching players data:', error);
@@ -376,11 +378,10 @@ const GetPlayers = () => {
     return (
         <div className="bg-white py-6 sm:py-8 lg:py-12">
             <div className="mx-auto max-w-screen-lg px-4 md:px-8">
-                <div
-                    className="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
+                <div className="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
                     <div>
                         <h2 className="text-xl font-bold text-indigo-500 md:text-2xl">Fonction : Liste des participants </h2>
-                        <p className="text-gray-600">Permet de : Récupérer la liste des joueurs qui possède actuellement un ticket</p>
+                        <p className="text-gray-600">Permet de : Récupérer la liste des joueurs qui possèdent actuellement un ticket</p>
                     </div>
                     <button
                         className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
@@ -389,15 +390,24 @@ const GetPlayers = () => {
                         Exécuter
                     </button>
                 </div>
-                {playersData.length > 0 && (
-                    <div className="mt-8 bg-gray-200 p-6 rounded-lg">
-                        <h2 className="text-xl font-bold mb-4">Résultat :</h2>
-                        <ul>
-                            {playersData.map((player, index) => (
-                                <li key={index} className="text-gray-800">{player}</li>
-                            ))}
-                        </ul>
-                    </div>
+                {buttonClicked && (
+                    <>
+                        {playersData.length > 0 ? (
+                            <div className="mt-8 bg-gray-200 p-6 rounded-lg">
+                                <h2 className="text-xl font-bold mb-4">Résultat :</h2>
+                                <ul>
+                                    {playersData.map((player, index) => (
+                                        <li key={index} className="text-gray-800">{player}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : (
+                            <div className="mt-8 bg-gray-200 p-6 rounded-lg">
+                                <h2 className="text-xl font-bold mb-4">Résultat :</h2>
+                                <p className="text-gray-800">Aucun joueur n&apos;a encore acheté de ticket</p>
+                            </div>
+                        )}
+                    </>
                 )}
             </div>
         </div>
